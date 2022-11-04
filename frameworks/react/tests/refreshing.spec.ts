65e5308efb58abefa68fb25ea3d78926e364c9d8
François Wouts <f@zenc.io>
2022-11-04 22:18:25 +1100

    Cleanup

--- frameworks/react/tests/refreshing.spec.ts
+++ frameworks/react/tests/refreshing.spec.ts
@@ -6,19 +6,19 @@ import pluginFactory from "../src";
 test.describe.configure({ mode: "parallel" });
 
 test.describe("react/refreshing", () => {
-  let preview: Awaited<ReturnType<typeof startPreview>>;
-
   for (const version of [16, 17, 18]) {
-    test.beforeEach(async ({ page }) => {
-      preview = await startPreview(
-        [pluginFactory],
-        page,
-        path.join(__dirname, "../../../test-apps/react" + version)
-      );
-    });
-    test.afterEach(() => preview?.stop());
-
     test.describe(`v${version}`, () => {
+      let preview: Awaited<ReturnType<typeof startPreview>>;
+
+      test.beforeEach(async ({ page }) => {
+        preview = await startPreview(
+          [pluginFactory],
+          page,
+          path.join(__dirname, "../../../test-apps/react" + version)
+        );
+      });
+      test.afterEach(() => preview?.stop());
+
       test("renders top-level component", async () => {
         await preview.show("src/App.tsx:App");
         const iframe = await preview.iframe.get();
