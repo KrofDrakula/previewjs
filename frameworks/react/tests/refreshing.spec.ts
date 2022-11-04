711c39c424305ee10042e9724c8c4b3ec969d1d2
François Wouts <f@zenc.io>
2022-11-04 22:17:54 +1100

    More tests

--- frameworks/react/tests/refreshing.spec.ts
+++ frameworks/react/tests/refreshing.spec.ts
@@ -6,21 +6,35 @@ import pluginFactory from "../src";
 test.describe.configure({ mode: "parallel" });
 
 test.describe("react/refreshing", () => {
-  let preview: Awaited<ReturnType<typeof startPreview>> | null = null;
-
-  test.afterEach(() => preview?.stop());
+  let preview: Awaited<ReturnType<typeof startPreview>>;
 
   for (const version of [16, 17, 18]) {
+    test.beforeEach(async ({ page }) => {
+      preview = await startPreview(
+        [pluginFactory],
+        page,
+        path.join(__dirname, "../../../test-apps/react" + version)
+      );
+    });
+    test.afterEach(() => preview?.stop());
+
     test.describe(`v${version}`, () => {
-      test("renders top-level component", async ({ page }) => {
-        preview = await startPreview(
-          [pluginFactory],
-          page,
-          path.join(__dirname, "../../../test-apps/react" + version)
-        );
+      test("renders top-level component", async () => {
         await preview.show("src/App.tsx:App");
         const iframe = await preview.iframe.get();
         await iframe.waitForSelector(".App-logo");
+      });
+
+      test("renders top-level component after file system change", async () => {
+        await preview.show("src/App.tsx:App");
+        const iframe = await preview.iframe.get();
+        await iframe.waitForSelector(".App-logo");
+        await preview.fileManager.update("src/App.tsx", {
+          kind: "edit",
+          search: `className="App"`,
+          replace: `className="App-modified"`,
+        });
+        await iframe.waitForSelector(".App-modified");
       });
     });
   }
