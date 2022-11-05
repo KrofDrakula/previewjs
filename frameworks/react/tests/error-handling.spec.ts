51e171f4ef7d8c26d691bb6ff293428904cefe0f
François Wouts <f@zenc.io>
2022-11-05 20:52:05 +1100

    one more

--- frameworks/react/tests/error-handling.spec.ts
+++ frameworks/react/tests/error-handling.spec.ts
@@ -53,6 +53,56 @@ for (const reactVersion of [16, 17, 18]) {
         // The component should still be shown.
         await preview.iframe.waitForSelector(".App");
       });
+
+      test("fails correctly when encountering broken module imports before update", async () => {
+        await preview.fileManager.update(
+          "src/App.tsx",
+          `import logo from "some-module";
+
+          export function App() {
+            return <div>{logo}</div>;
+          }`
+        );
+        const events: PreviewEvent[] = [];
+        preview.listen((e) => events.push(e));
+        await preview.show("src/App.tsx:App");
+        await preview.iframe.waitForExpectedIframeRefresh();
+        expect(events.filter((e) => e.kind === "log-message")).toEqual([
+          {
+            kind: "log-message",
+            level: "error",
+            message: expect.stringContaining(
+              `Failed to resolve import "some-module" from "src${path.sep}App.tsx". Does the file exist?`
+            ),
+            timestamp: expect.anything(),
+          },
+          {
+            kind: "log-message",
+            level: "error",
+            message: expect.stringContaining(
+              "Failed to fetch dynamically imported module"
+            ),
+            timestamp: expect.anything(),
+          },
+          {
+            kind: "log-message",
+            level: "error",
+            message: expect.stringContaining(
+              "Failed to fetch dynamically imported module"
+            ),
+            timestamp: expect.anything(),
+          },
+        ]);
+        await preview.fileManager.update(
+          "src/App.tsx",
+          `import logo from "./logo.svg";
+
+          export function App() {
+            return <div id="recovered">{logo}</div>;
+          }`
+        );
+        await preview.iframe.waitForSelector("#recovered");
+      });
     });
   });
 }
