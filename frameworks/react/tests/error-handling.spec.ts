a0a64968a7b3cd0e1a10c187828b7abd87d57afa
François Wouts <f@zenc.io>
2022-11-07 13:29:16 +1100

    logic not syntax

--- frameworks/react/tests/error-handling.spec.ts
+++ frameworks/react/tests/error-handling.spec.ts
@@ -227,7 +227,7 @@ for (const reactVersion of [16, 17, 18]) {
         await preview.iframe.waitForSelector("#recovered");
       });
 
-      test("fails correctly when encountering broken syntax (case 3)", async () => {
+      test("fails correctly when encountering broken logic", async () => {
         await preview.show("src/App.tsx:App");
         await preview.iframe.waitForSelector(".App");
         await preview.fileManager.update(
